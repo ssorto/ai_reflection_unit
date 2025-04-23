26d6abc85299d3dafc9d3a2bb5a4733852631b0f
ssorto <stephanie.sorto-moreno@duke.edu>
2025-04-23 02:10:30 +0100

    Update color mappings and HSV ranges for skeptical -> orange attempt

--- project_files/utils.py
+++ project_files/utils.py
@@ -4,7 +4,7 @@ emotion_to_color = {
     "Relieved": "#6EC1E4",
     "Overwhelmed": "#6B3FA0",
     "Confident": "#F7B500",
-    "Skeptical": "#708090",
+    "Skeptical": "#FFA500", #new
     "Satisfied": "#50C878",
     "Frustrated": "#E63946"
 }
@@ -13,7 +13,7 @@ color_to_emotion = {
     "blue": "Relieved",
     "purple": "Overwhelmed",
     "yellow": "Confident",
-    "gray": "Skeptical",
+    "orange": "Skeptical", #new
     "green": "Satisfied",
     "red": "Frustrated"
 }
